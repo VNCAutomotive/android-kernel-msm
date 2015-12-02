388fe2cc25dbea1dad9d2d66d5a8240323ab798b
Felipe Balbi <balbi@ti.com>
2015-12-02 10:06:45 -0600

    usb: dwc3: gadget: handle request->zero
    So far, dwc3 has always missed request->zero
    handling for every endpoint. Let's implement
    that so we can handle cases where transfer must
    be finished with a ZLP.
    
    Note that dwc3 is a little special. Even though
    we're dealing with a ZLP, we still need a buffer
    of wMaxPacketSize bytes; to hide that detail from
    every gadget driver, we have a preallocated buffer
    of 1024 bytes (biggest bulk size) to use (and
    share) among all endpoints.
    
    Reported-by: Ravi B <ravibabu@ti.com>
    Signed-off-by: Felipe Balbi <balbi@ti.com>

--- drivers/usb/dwc3/gadget.c
+++ drivers/usb/dwc3/gadget.c
@@ -1202,6 +1202,31 @@ static int __dwc3_gadget_ep_queue(struct dwc3_ep *dep, struct dwc3_request *req)
 	return 0;
 }
 
+static void __dwc3_gadget_ep_zlp_complete(struct usb_ep *ep,
+		struct usb_request *request)
+{
+	dwc3_gadget_ep_free_request(ep, request);
+}
+
+static int __dwc3_gadget_ep_queue_zlp(struct dwc3 *dwc, struct dwc3_ep *dep)
+{
+	struct dwc3_request		*req;
+	struct usb_request		*request;
+	struct usb_ep			*ep = &dep->endpoint;
+
+	request = dwc3_gadget_ep_alloc_request(ep, GFP_ATOMIC);
+	if (!request)
+		return -ENOMEM;
+
+	request->length = 0;
+	request->buf = dwc->zlp_buf;
+	request->complete = __dwc3_gadget_ep_zlp_complete;
+
+	req = to_dwc3_request(request);
+
+	return __dwc3_gadget_ep_queue(dep, req);
+}
+
 static int dwc3_gadget_ep_queue(struct usb_ep *ep, struct usb_request *request,
 	gfp_t gfp_flags)
 {
@@ -1229,6 +1254,16 @@ static int dwc3_gadget_ep_queue(struct usb_ep *ep, struct usb_request *request,
 		"trying to queue unaligned request (%d)\n", request->length);
 
 	ret = __dwc3_gadget_ep_queue(dep, req);
+
+	/*
+	 * Okay, here's the thing, if gadget driver has requested for a ZLP by
+	 * setting request->zero, instead of doing magic, we will just queue an
+	 * extra usb_request ourselves so that it gets handled the same way as
+	 * any other request.
+	 */
+	if (ret == 0 && request->zero && (request->length % ep->maxpacket == 0))
+		ret = __dwc3_gadget_ep_queue_zlp(dwc, dep);
+
 	spin_unlock_irqrestore(&dwc->lock, flags);
 
 	return ret;
@@ -2771,6 +2806,12 @@ int __devinit dwc3_gadget_init(struct dwc3 *dwc)
 		goto err3;
 	}
 
+	dwc->zlp_buf = kzalloc(DWC3_ZLP_BUF_SIZE, GFP_KERNEL);
+	if (!dwc->zlp_buf) {
+		ret = -ENOMEM;
+		goto err4;
+	}
+
 	dev_set_name(&dwc->gadget.dev, "gadget");
 
 	switch (dwc->maximum_speed) {
@@ -2809,7 +2850,7 @@ int __devinit dwc3_gadget_init(struct dwc3 *dwc)
 
 	ret = dwc3_gadget_init_endpoints(dwc);
 	if (ret)
-		goto err4;
+		goto err5;
 
 	irq = platform_get_irq(to_platform_device(dwc->dev), 0);
 
@@ -2818,7 +2859,7 @@ int __devinit dwc3_gadget_init(struct dwc3 *dwc)
 	if (ret) {
 		dev_err(dwc->dev, "failed to request irq #%d --> %d\n",
 				irq, ret);
-		goto err5;
+		goto err6;
 	}
 
 	reg = dwc3_readl(dwc->regs, DWC3_DCFG);
@@ -2858,13 +2899,13 @@ int __devinit dwc3_gadget_init(struct dwc3 *dwc)
 	if (ret) {
 		dev_err(dwc->dev, "failed to register gadget device\n");
 		put_device(&dwc->gadget.dev);
-		goto err6;
+		goto err7;
 	}
 
 	ret = usb_add_gadget_udc(dwc->dev, &dwc->gadget);
 	if (ret) {
 		dev_err(dwc->dev, "failed to register udc\n");
-		goto err7;
+		goto err8;
 	}
 
 	if (dwc->dotg) {
@@ -2872,7 +2913,7 @@ int __devinit dwc3_gadget_init(struct dwc3 *dwc)
 		ret = otg_set_peripheral(&dwc->dotg->otg, &dwc->gadget);
 		if (ret) {
 			dev_err(dwc->dev, "failed to set peripheral to otg\n");
-			goto err7;
+			goto err8;
 		}
 	} else {
 		pm_runtime_no_callbacks(&dwc->gadget.dev);
@@ -2883,15 +2924,18 @@ int __devinit dwc3_gadget_init(struct dwc3 *dwc)
 
 	return 0;
 
-err7:
+err8:
 	device_unregister(&dwc->gadget.dev);
 
-err6:
+err7:
 	dwc3_writel(dwc->regs, DWC3_DEVTEN, 0x00);
 	free_irq(irq, dwc);
 
-err5:
+err6:
 	dwc3_gadget_free_endpoints(dwc);
+
+err5:
+	kfree(dwc->zlp_buf);
 
 err4:
 	dma_free_coherent(dwc->dev, DWC3_EP0_BOUNCE_SIZE,
@@ -2933,6 +2977,7 @@ void dwc3_gadget_exit(struct dwc3 *dwc)
 			dwc->ep0_bounce, dwc->ep0_bounce_addr);
 
 	kfree(dwc->setup_buf);
+	kfree(dwc->zlp_buf);
 
 	dma_free_coherent(dwc->dev, sizeof(*dwc->ep0_trb),
 			dwc->ep0_trb, dwc->ep0_trb_addr);
